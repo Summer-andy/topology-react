99f3c2fd7909a676728b7f01703904ceb8360750
xdSi <3450236968@qq.com>
2020-10-26 08:28:45 +0800

    docs: 修改README.md 更新功能

---
+++ src/Layout/component/backgroundComponent/LayoutComponent/index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Form, Row, Col, InputNumber, Button } from 'antd';
+import { layout, spaceBetween, alignNodes } from '@topology/layout';
+import { canvas } from '../../../index';
+const formLayout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 15 }
+};
+const Layout = ({ form: { getFieldDecorator, validateFields } }) => {
+
+  const startLayout = () => {
+    validateFields((err, value) => {
+      if(err) return;
+      if(canvas) {
+        console.log(value);
+        console.log(canvas.data.pens);
+        layout(canvas.data.pens, value);
+        console.log(canvas.data.pens);
+        canvas.updateProps(true, canvas.data.pens);
+      }
+    });
+  }
+
+  return (
+    <Form {...formLayout} style={{ margin: '10px 10px' }}>
+      <Row>
+        <Col span={24}>
+          <Form.Item label="最大宽度">
+            {getFieldDecorator('maxWidth', {
+              initialValue: 1000
+            })(<InputNumber style={{ width: '100%' }} min={0} placeholder="请输入最大宽度" />)}
+          </Form.Item>
+        </Col>
+        <Col span={24}>
+          <Form.Item label="节点宽度">
+            {getFieldDecorator('nodeWidth', {
+              initialValue: 0
+            })(<InputNumber style={{ width: '100%' }} min={0} placeholder="请输入节点宽度" />)}
+          </Form.Item>
+        </Col>
+        <Col span={24}>
+          <Form.Item label="节点高度">
+            {getFieldDecorator('nodeHeight', {
+              initialValue: 0
+            })(<InputNumber style={{ width: '100%' }} min={0} placeholder="请输入节点高度" />)}
+          </Form.Item>
+        </Col>
+
+        <Col span={24}>
+          <Form.Item label="水平个数">
+            {getFieldDecorator('maxCount', {
+              initialValue: 0
+            })(<InputNumber style={{ width: '100%' }} min={0} placeholder="请输入水平个数" />)}
+          </Form.Item>
+        </Col>
+
+        <Col span={24}>
+          <Form.Item label="水平间距">
+            {getFieldDecorator('spaceWidth', {
+              initialValue: 30
+            })(<InputNumber style={{ width: '100%' }} min={0} placeholder="请输入水平间距" />)}
+          </Form.Item>
+        </Col>
+        <Col span={24}>
+          <Form.Item label="垂直间距">
+            {getFieldDecorator('spaceHeight', {
+              initialValue: 30
+            })(<InputNumber style={{ width: '100%' }} min={0} placeholder="请输入垂直间距" />)}
+          </Form.Item>
+        </Col>
+
+        <Col span={24}>
+          <Button
+            type="primary"
+            style={{ marginLeft: 22, width: 245 }}
+            onClick={() => startLayout()}
+          >
+            开始排版
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default Form.create()(Layout);
--- src/Layout/component/backgroundComponent/index.js
+++ src/Layout/component/backgroundComponent/index.js
@@ -1,15 +1,17 @@
 import React, { useMemo, useEffect, useState } from 'react';
-import { Form, Tabs, Row, Col, Input, Collapse, Button } from 'antd';
+import { Form, Tabs, Row, Col, Input, Collapse, Button, Switch } from 'antd';
 import './index.css';
 import MQTTComponent from './MQTTComponent';
+import LayoutComponent from './LayoutComponent';
 import { canvas } from '../../index';
 const { TabPane } = Tabs;
 const { Panel } = Collapse;
 const { TextArea } = Input;
 const CanvasProps = ({ data, form: { getFieldDecorator }, form }) => {
-
   const { bkColor, bkImage } = data.data;
   const [wsAddress, setWsAddress] = useState('ws://123.207.136.134:9010/ajaxchattest');
+
+  console.log(canvas.layout);
 
   useEffect(() => {
     form.validateFields((err, value) => {
@@ -19,73 +21,96 @@ const CanvasProps = ({ data, form: { getFieldDecorator }, form }) => {
       data.data.bkImage = value.bkImage;
       data.render();
       form.resetFields();
-    })
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [form])
+  }, [form]);
 
   /**
-  * 渲染位置和大小的表单
-  */
+   * 渲染位置和大小的表单
+   */
 
   const renderForm = useMemo(() => {
     const formLayout = {
-      labelCol: { span: 8 },
-      wrapperCol: { span: 15 },
+      labelCol: { span: 7 },
+      wrapperCol: { span: 15 }
     };
-    return <Form {...formLayout}>
-      <Row>
-        <Col span={24}>
-          <Form.Item label="背景颜色">
-            {getFieldDecorator('bkColor', {
-              initialValue: bkColor
-            })(<Input type="color" />)}
-          </Form.Item>
-        </Col>
-        <Col span={24}>
-          <Form.Item label="背景图片">
-            {getFieldDecorator('bkImage', {
-              initialValue: bkImage
-            })(<Input placeholder="请输入图片的地址" />)}
-          </Form.Item>
-        </Col>
-      </Row>
-    </Form>
+    return (
+      <Form {...formLayout} style={{ marginTop: 10 }}>
+        <Row>
+          <Col span={24}>
+            <Form.Item label="背景颜色">
+              {getFieldDecorator('bkColor', {
+                initialValue: bkColor
+              })(<Input type="color" />)}
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item label="背景图片">
+              {getFieldDecorator('bkImage', {
+                initialValue: bkImage
+              })(<Input placeholder="请输入图片的地址" />)}
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item label="背景网格">
+              {getFieldDecorator('grid', {
+                initialValue: true
+              })(
+                <Switch
+                  checkedChildren="开"
+                  unCheckedChildren="关"
+                  onClick={(e) => {
+                    console.log(e);
+                    canvas.showGrid(e);
+                  }}
+                />
+              )}
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    );
   }, [bkColor, bkImage, getFieldDecorator]);
 
-
   /**
-  * 发起websocket连接
-  */
+   * 发起websocket连接
+   */
 
   const onHandleConnectWS = () => {
     canvas.openSocket(wsAddress);
-  }
+  };
 
   return (
     <div className="rightArea">
       <Tabs defaultActiveKey="1">
         <TabPane tab="图文设置" key="1" style={{ margin: 0 }}>
-          <Collapse defaultActiveKey={['1']}>
-            <Panel header="样式" key="1">
-              {
-                renderForm
-              }
-            </Panel>
-          </Collapse>
+          {renderForm}
         </TabPane>
         <TabPane tab="消息通信" key="2" style={{ margin: 0 }}>
           <Collapse defaultActiveKey={['1', '2']}>
             <Panel header="websocket地址" key="1">
-                <TextArea placeholder="请输入websocket地址" value={wsAddress} onChange={e => setWsAddress(e.target.value)} />
-                <Button type="primary" style={{ width: 265, marginTop: 10 }} onClick={() => onHandleConnectWS()}>测试连接</Button>
+              <TextArea
+                placeholder="请输入websocket地址"
+                value={wsAddress}
+                onChange={(e) => setWsAddress(e.target.value)}
+              />
+              <Button
+                type="primary"
+                style={{ width: 265, marginTop: 10 }}
+                onClick={() => onHandleConnectWS()}
+              >
+                测试连接
+              </Button>
             </Panel>
             <Panel header="MQTT地址" key="2">
               <MQTTComponent />
             </Panel>
           </Collapse>
         </TabPane>
+        <TabPane tab="排版布局" key="3" style={{ margin: 0 }}>
+          <LayoutComponent />
+        </TabPane>
       </Tabs>
-
     </div>
   );
 };
